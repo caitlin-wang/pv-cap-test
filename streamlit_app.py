3d705e82f93e6ac77e1fed20fdcd1be36c1ffa25
caitlin-wang <caitlin.wang@recurrentenergy.com>
2024-11-06 21:40:50 +0000

    added all inputs, changed data inputs on tab2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,56 @@
 import streamlit as st
+import datetime
 import pandas as pd
+from zipfile import ZipFile
 
 st.set_page_config(page_title= "PV Cap Test", page_icon="icon.jpg", layout="centered")
 st.logo("long_logo.jpg", icon_image="icon.jpg")
 st.sidebar.subheader(" ")
 st.title("PV Cap Test")
 
-tab1, tab2, tab3 = st.tabs(['Measured', 'PVSyst', 'Report'])
+tab1, tab2, tab3 = st.tabs(['Inputs', 'Data Upload', 'Report'])
 
-form = tab1.form("input form")
-form.header("Inputs:")
+form1 = tab1.form("inputs form")
 
-uploaded_files = form.file_uploader("Upload MET Data", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+form1.subheader("Irradiance Inputs:")
+form1_col1, form1_col2 = form1.columns(2)
+test_start_date = form1_col1.date_input("Start Date", 'today', format='MM/DD/YYYY')
+test_end_date = form1_col2.date_input("End Date", 'today', format='MM/DD/YYYY')
+minimum_irradiance = form1_col1.number_input("Minimum Irradiance (W/m^2):", min_value=0, value=400, step=100)
+max_irradiance = form1_col2.number_input("Maximum Irradiance (W/m^2):", min_value=minimum_irradiance, value=1500, step=100)
+temporal_stability_thresold = form1_col1.number_input("Temporal Stability Threshold:", min_value=0, value=20, step=1)
+spatial_stability_thresold = form1_col2.number_input("Spatial Stability Threshold:", min_value=0.0, value=0.20, step=0.10)
 
-uploaded_files = form.file_uploader("Upload Inverter Data", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+form1.subheader("Grid Inputs:")
+form1_col1, form1_col2 = form1.columns(2)
+minimum_grid = form1_col1.number_input("Minimum Grid Value:", value=0, step=100)
+max_gridlimit = form1_col2.number_input("Maximum Grid Value:", value=99600, step=100)
+grid_clipping_thresold = form1.number_input("Grid Clipping Threshold:", value=0.98, step=0.01)
+grid_clipping = grid_clipping_thresold * max_gridlimit
 
-uploaded_files = form.file_uploader("Upload Meter Data", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+form1.subheader("RC Inputs:")
+form1_col1, form1_col2 = form1.columns(2)
+percentile = form1_col1.number_input("Percentile:", min_value=0.0, max_value=1.0, value=0.50, step=0.10)
+reporting_condition_thresold = form1_col2.number_input("Reporting Condition Threshold:", value=0.20, min_value=0.0, step=0.01)
 
-uploaded_files = form.file_uploader("Upload Column Groups", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+form1.subheader("Inverter Inputs:")
+form1_col1, form1_col2 = form1.columns(2)
+inverter_rating = form1_col1.number_input("Inverter Rating:", min_value=0, value=3600, step=100)
+Inverter_limit = form1_col2.number_input("Inverter Limit:", min_value=0, value=118800, step=100)
+inverter_clipping_thresold = form1_col1.number_input("Inverter Clipping Threshold:", min_value=0.0, value=0.98, max_value=1.0, step=0.01)
+inverter_clipping = inverter_rating * inverter_clipping_thresold
 
-form.form_submit_button("Submit Files")
+form1.subheader("Other Inputs:")
+pvsyst_shading = form1.number_input("PVSyst Shading:", min_value=0, value=1, step=1)
+bifaciality = form1.number_input("Bifaciality", value=0.7, min_value=0.0, max_value=1.0, step=0.1)
+availability_min_fpoa = form1.number_input("Availability Minimum FPOA", value=50, min_value=0, step=1)
+system_size_dc = form1.number_input("System Size DC", value=134046, min_value=0, step=1)
+
+form1.form_submit_button("Submit Inputs")
+
+uploaded_zip = tab2.file_uploader("Upload raw data", type='zip')
+column_groups = tab2.file_uploader("Upload column groups", type=['csv','xlsx'])
+pvsyst_test_model_path = tab2.file_uploader("Upload PVSyst test model", type=['csv'])
+
+if uploaded_zip is not None:
+    ZipFile(uploaded_zip.name, 'r').extractall()
