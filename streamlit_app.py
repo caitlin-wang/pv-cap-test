acec9150c9d576f631f49125283686be81ec51d5
caitlin-wang <caitlin.wang@recurrentenergy.com>
2024-11-06 16:45:39 +0000

    set up 3 tabs, measured data inputs, uploaded logo jpgs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,38 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title= "PV Cap Test", page_icon="icon.jpg", layout="centered")
+st.logo("long_logo.jpg", icon_image="icon.jpg")
+st.sidebar.subheader(" ")
+st.title("PV Cap Test")
+
+tab1, tab2, tab3 = st.tabs(['Measured', 'PVSyst', 'Report'])
+
+form = tab1.form("input form")
+form.header("Inputs:")
+
+uploaded_files = form.file_uploader("Upload MET Data", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    form.write("filename:", uploaded_file.name)
+    form.write(bytes_data)
+
+uploaded_files = form.file_uploader("Upload Inverter Data", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    form.write("filename:", uploaded_file.name)
+    form.write(bytes_data)
+
+uploaded_files = form.file_uploader("Upload Meter Data", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    form.write("filename:", uploaded_file.name)
+    form.write(bytes_data)
+
+uploaded_files = form.file_uploader("Upload Column Groups", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    form.write("filename:", uploaded_file.name)
+    form.write(bytes_data)
+
+form.form_submit_button("Submit Files")
