174b5df7521196dc05f26be13c5b659eb402166b
caitlin-wang <caitlin.wang@recurrentenergy.com>
2024-12-09 03:26:48 +0000

    change fpoa_data to df[fpoa_data] in filter_fpoa_blank

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,13 +50,13 @@ def filter_grid_clipping(df):
     return df['average_meter_data'] < grid_clipping
 
 def filter_inverter_clipping(df, inverter_data):
-    return inverter_data.apply(lambda row: row.max() < inverter_clipping, axis=1)
+    return inverter_data    .apply(lambda row: row.max() < inverter_clipping, axis=1)
 
 def filter_inverter_zero(df, inverter_data):
     return ~(inverter_data == 0).any(axis=1)
     
 def filter_fpoa_blank(df, fpoa_data):
-    return ~(fpoa_data.isnull()).any(axis=1)
+    return ~(df[fpoa_data].isnull()).any(axis=1)
 
 def filter_fpoa_zero(df, fpoa_data):
     return ~(fpoa_data == 0).any(axis=1)
@@ -85,8 +85,8 @@ def filter_fpoa_qc(df, minimum_irradiance, max_irradiance):
 def filter_spatial_stability(df, fpoa_data, spatial_threshold):
     #df['average_fpoa_pct_change'] = df['average_fpoa'].pct_change() * 100  #Ask Ashish if we need this
     # df['temporal_stability_check'] = df['average_fpoa_pct_change'].abs() <= temporal_threshold #Ask Ashish if we need this
-    df['fpoa_spatial_std'] = fpoa_data.std(axis=1)
-    df['fpoa_spatial_mean'] = fpoa_data.mean(axis=1)
+    df['fpoa_spatial_std'] = df[fpoa_data].std(axis=1) # changed fpoa_data to df[fpoa_data]
+    df['fpoa_spatial_mean'] = df[fpoa_data].mean(axis=1) # changed fpoa_data to df[fpoa_data]
     df['fpoa_spatial_stability'] = df['fpoa_spatial_std'] / df['fpoa_spatial_mean']
     df['spatial_stability_check'] = df['fpoa_spatial_stability'].abs() <= spatial_threshold
     return df['spatial_stability_check']
