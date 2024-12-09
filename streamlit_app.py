84da58c7ff7f4a2db695c3193b9f0282341bd49f
caitlin-wang <caitlin.wang@recurrentenergy.com>
2024-12-09 03:42:12 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,9 @@ def filter_grid_clipping(df):
     return df['average_meter_data'] < grid_clipping
 
 def filter_inverter_clipping(df, inverter_data):
-    return df[inverter_data].apply(lambda row: row.max() < inverter_clipping, axis=1)
+    #return df[inverter_data].apply(lambda row: row.max() < inverter_clipping, axis=1)
+    tab3.write(inverter_data)
+    tab3.write(df[inverter_data])
 
 def filter_inverter_zero(df, inverter_data):
     return ~(df[inverter_data] == 0).any(axis=1)
