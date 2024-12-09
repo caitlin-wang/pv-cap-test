ad787cd43eb58557e15636af6fc1add628e96548
caitlin-wang <caitlin.wang@recurrentenergy.com>
2024-12-09 15:18:08 +0000

    undo

--- streamlit_app.py
+++ streamlit_app.py
@@ -524,8 +524,8 @@ fpoa_wind, fpoa_temp, fpoa_poa_poa, fpoa = final_coefficients
 
 ## Note: Calculating energy 
 
-#measured_energy_bifacial = round(rc_poa_total*(fpoa+fpoa_poa_poa*rc_poa_total+fpoa_temp*rc_temp+fpoa_wind*rc_wind)[0])
-#measured_energy_monofacial = round(rc_fpoa*(fpoa+fpoa_poa_poa*rc_fpoa+fpoa_temp*rc_temp+fpoa_wind*rc_wind)[0])
+measured_energy_bifacial = round(rc_poa_total*(fpoa+fpoa_poa_poa*rc_poa_total+fpoa_temp*rc_temp+fpoa_wind*rc_wind)[0])
+measured_energy_monofacial = round(rc_fpoa*(fpoa+fpoa_poa_poa*rc_fpoa+fpoa_temp*rc_temp+fpoa_wind*rc_wind)[0])
 
 measured_regression_df["Energy Predicted"] = measured_regression_df['average_poa_total']*((fpoa)+fpoa_poa_poa*measured_regression_df['average_poa_total']+fpoa_temp*measured_regression_df['average_temp']+fpoa_wind*1)
 
