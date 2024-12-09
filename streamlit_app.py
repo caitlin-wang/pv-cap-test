1ab8cb9746b2a1667a09eb27c983f8a613a08ae0
caitlin-wang <caitlin.wang@recurrentenergy.com>
2024-12-09 03:46:48 +0000

    revert inverter funcs

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,12 +50,11 @@ def filter_grid_clipping(df):
     return df['average_meter_data'] < grid_clipping
 
 def filter_inverter_clipping(df, inverter_data):
-    #return df[inverter_data].apply(lambda row: row.max() < inverter_clipping, axis=1)
-    #tab3.write(inverter_data)
-    tab3.write(df[inverter_data])
+    #return inverter_data.apply(lambda row: row.max() < inverter_clipping, axis=1)
+    tab3.write(inverter_data)
 
 def filter_inverter_zero(df, inverter_data):
-    return ~(df[inverter_data] == 0).any(axis=1)
+    return ~(inverter_data == 0).any(axis=1)
     
 def filter_fpoa_blank(df, fpoa_data):
     return ~(df[fpoa_data].isnull()).any(axis=1)
