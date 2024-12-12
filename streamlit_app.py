4a251fb145fcfc06219299f1963b818f86909467
caitlin-wang <caitlin.wang@recurrentenergy.com>
2024-12-12 16:35:07 +0000

    fix2

--- streamlit_app.py
+++ streamlit_app.py
@@ -272,7 +272,7 @@ filter_results_df = pd.DataFrame(filter_results).set_index('Filter Description')
 # Apply filters and store the results in new columns
 merged_df['meter>0'] = filters.filter_meter_greater_zero(merged_df, minimum_grid)
 merged_df['grid_clipping'] = filters.filter_grid_clipping(merged_df, grid_clipping)
-merged_df['inverter_clipping_check'] = filters.filter_inverter_clipping(merged_df, inverter_df)
+merged_df['inverter_clipping_check'] = filters.filter_inverter_clipping(merged_df, inverter_df, inverter_clipping)
 merged_df['inverter_blank'] = filters.filter_inverter_zero(merged_df, inverter_df)
 merged_df['inverter_zero'] = filters.filter_inverter_zero(merged_df, inverter_df)
 merged_df['fpoa_blank'] = filters.filter_fpoa_blank(merged_df, fpoa_data)
