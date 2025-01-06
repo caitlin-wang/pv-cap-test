32ac1c07bf447aa39028fabdc0dc66f0341cfcf0
caitlin-wang <caitlin.wang@recurrentenergy.com>
2025-01-06 18:59:44 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -487,7 +487,6 @@ measured_regression_df['fpoaxwind'] = measured_regression_df['average_wind'] * m
 
 X = measured_regression_df[['fpoa','fpoaxfpoa','fpoaxtemp','fpoaxwind']]
 y = measured_regression_df['average_meter_data']
-y = measured_regression_df[vars.meter_data]
 
 coefficients, residuals, rank, s = np.linalg.lstsq(X, y, rcond=None)
 final_coefficients = coefficients[::-1]
@@ -497,8 +496,8 @@ fpoa_wind, fpoa_temp, fpoa_poa_poa, fpoa = final_coefficients
 ##Power = POA * (fpoa + fpoa_poa_poa*POA + fpoa_temp*Temp + fpoa_wind*Wind)
 ## Note: Calculating energy 
 
-measured_energy_bifacial = round(rc_poa_total*(fpoa+fpoa_poa_poa*rc_poa_total+fpoa_temp*rc_temp+fpoa_wind*rc_wind)[0])
-measured_energy_monofacial = round(rc_fpoa*(fpoa+fpoa_poa_poa*rc_fpoa+fpoa_temp*rc_temp+fpoa_wind*rc_wind)[0])
+measured_energy_bifacial = round(rc_poa_total*(fpoa+fpoa_poa_poa*rc_poa_total+fpoa_temp*rc_temp+fpoa_wind*rc_wind))
+measured_energy_monofacial = round(rc_fpoa*(fpoa+fpoa_poa_poa*rc_fpoa+fpoa_temp*rc_temp+fpoa_wind*rc_wind))
 
 measured_regression_df["Energy Predicted"] = measured_regression_df['average_poa_total']*((fpoa)+fpoa_poa_poa*measured_regression_df['average_poa_total']+fpoa_temp*measured_regression_df['average_temp']+fpoa_wind*1)
 
