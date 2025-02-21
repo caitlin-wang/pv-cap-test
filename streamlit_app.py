7bc003012ddcba3853269159315409a11d7cc6cf
caitlin-wang <caitlin.wang@recurrentenergy.com>
2025-02-21 17:05:00 +0000

    add coeffs to rc loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -230,15 +230,16 @@ def loop_rc_threshold(min_rc, max_rc, step_size, rc_poa_total, merged_df):
             'Excluding Count': count_excluding,
             'Secondary Above RC %': secondary_above_rc_perc,
             'Secondary Below RC %': secondary_below_rc_perc,
+            'FPOA Coeff' : fpoa, 
+            'FPOAxFPOA Coeff' : fpoa_poa_poa, 
+            'FPOAxTemp Coeff' : fpoa_temp, 
+            'FPOAxWind Coeff' : fpoa_wind,
             'Measured Energy Bi': measured_energy_bifacial,
             'Expected Energy Bi': expected_energy_bifacial,
             'Capacity Ratio Monofacial': Capcity_Ratio_Mono, 
             'Capacity Ratio Bifacial': Capcity_Ratio_Bifacial 
         }
-        # 'FPOA Coeff' : fpoa, 
-        # 'FPOAxFPOA Coeff' : fpoa_poa_poa, 
-        # 'FPOAxTemp Coeff' : fpoa_temp, 
-        # 'FPOAxWind Coeff' : fpoa_wind,
+        # 
         # 'Measured Energy Mono': measured_energy_monofacial, 
         # 'Measured Energy Bi' : measured_energy_bifacial, 
 
